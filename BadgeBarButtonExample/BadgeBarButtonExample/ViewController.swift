933d1d85a857913731dddec71b88f982a2e8b82f
Paulius Vindzigelskis <p.vindzigelskis@gmail.com>
2018-02-12 11:57:54 -0600

    example app

---
+++ BadgeBarButtonExample/BadgeBarButtonExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BadgeBarButtonExample
+//
+//  Created by Vindzigelskis, Paulius on 2/12/18.
+//  Copyright © 2018 New Guy Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
